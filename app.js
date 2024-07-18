de0c1f8c8d8edb40293846b8972e9e184a1a2aea
sedat0101 <sfsi_0101@hotmail.com>
2024-07-18 15:06:05 +0300

    courses

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const pageRoute = require('./routes/pageRoute')
+const courseRoute = require('./routes/courseRoutes')
 
 const app = express()
+
+// Connet DB
+mongoose.connect('mongodb://localhost/smartedu-db')
+.then(() => {
+    console.log("DB Connected Successfuly")
+})
+
 
 //Template Engine
 app.set("view engine", "ejs")
 
 //Middlewares
 app.use(express.static("public"))
+// Bu middleware'ler olmadan create formu çalışamaz
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 // Routes
-app.get("/", (req, res) => {
-    res.render("index", {
-        page_name: "index"
-    })
-})
-app.get("/about", (req, res) => {
-    res.render("about", {
-        page_name: "about"
-    })
-})
+app.use("/", pageRoute)
+app.use("/courses", courseRoute)
 
 const port = 3000
 app.listen(port, () => {
