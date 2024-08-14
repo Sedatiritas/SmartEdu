04e727c8ce3f041dc240d3815bf83bc9cdf58825
sedat0101 <sfsi_0101@hotmail.com>
2024-08-14 13:18:09 +0300

    admin panel

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,10 +48,26 @@ exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({_id:req.session.userID}).populate('courses')
   const categories = await Category.find()
   const courses = await Course.find({user:req.session.userID})
+  const users = await User.find()
   res.render("dashboard", {
       page_name: "dashboard",
       user,
       categories,
-      courses
+      courses,
+      users
   })
 }
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndRemove(req.params.id);
+    await Course.deleteMany({ user: req.params.id });
+    req.flash("error", `${user.name} user has been removed successfully.`);
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
